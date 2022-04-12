e9f55624d6e4f85ccf243fad1c63a1c31dcce161
isakbet <yosephbet@gmail.com>
2022-04-12 12:00:46 +0200

    add react-webshop template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      Webshop in React
+    </div>
+  );
+}
+
+export default App;
