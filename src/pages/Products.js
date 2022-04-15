116e8d56daefa55b24f5ea75e8ddf1431447ba67
isakbet <yosephbet@gmail.com>
2022-04-15 12:24:10 +0200

    add a list of all products retrieved via an API

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 function Products() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchProducts = async () => {
+    const data = await fetch('https://codexplained.se/sportstuff.php')
+    const products = await data.json()
+    setProducts(products)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
-    <div>Products</div>
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <section>
+          {products.map((product) => (
+            <div key={product.id}>
+              <h2>{product.title}</h2>
+              <img src={product.url} alt={product.url} />
+              <p>{product.price}</p>
+              <Link to={`/product/${product.id}`}>
+                <button>View</button>
+              </Link>
+            </div>
+          ))}
+        </section>
+      )}
+    </div>
   )
 }
 
