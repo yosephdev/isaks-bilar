cb0b8c51af28c6e2c245eb44b65997010d11ad09
isakbet <yosephbet@gmail.com>
2022-04-15 11:32:38 +0200

    import Link to products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Link to="/products">
+      Products
+    </Link>
   )
 }
 
