453ed06c5370e432162c0bcff5c438c026598d2f
isakbet <yosephbet@gmail.com>
2022-04-16 16:49:08 +0200

    add nav, ul and li tag to header tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 function Header() {
   return (
-    <Link to="/products">
-      Products
-    </Link>
+    <header>
+      <nav>
+        <ul>
+          {/* <li>
+            <Link to="/">Home</Link>
+          </li> */}
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          {/* <li><Link to="/checkout">Cart</Link></li> */}
+        </ul>
+      </nav>
+    </header>
   )
 }
 
