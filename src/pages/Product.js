b4bd476724e3181cc83d7423c2c1a51a21e60ba0
isakbet <yosephbet@gmail.com>
2022-04-16 16:51:40 +0200

    Display data of the selected product via API

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function Product() {
+  const params = useParams()
+  console.log(params)
+
+  const [product, setProduct] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  const fetchProduct = async () => {
+    const data = await fetch(
+      `https://codexplained.se/sportstuff.php?id=${params.productId}`
+    )
+    const product = await data.json()
+    setProduct(product)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchProduct()
+  }, [])
+
   return (
-    <div>Product</div>
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <section>
+          <h2>{params.id}</h2>
+          <img src={product.url} alt={product.url} />
+          <p>{product.price}</p>
+          <button>Add to cart</button>
+        </section>
+      )}
+    </div>
   )
 }
 
