ad361f69531ce99f84479c77731308013359d14a
isakbet <yosephbet@gmail.com>
2022-04-20 14:32:08 +0200

    add NavWrapper, style NavWrapper

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import styled from 'styled-components'
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          {/* <li>
-            <Link to="/">Home</Link>
-          </li> */}
-          <li>
-            <Link to="/products">Products</Link>
+    <NavWrapper className="Navbar navbar-expand-sm navbar-dark px-sm-5">
+      <div className="d-flex align-items-center justify-content-center">      
+        <ul className="navbar-nav align-item-center">
+          <li className="nav-item ml-5">
+            <Link to="/products"> Products </Link>
           </li>
-          {/* <li><Link to="/checkout">Cart</Link></li> */}
         </ul>
-      </nav>
-    </header>
+      </div>
+    </NavWrapper>
   )
 }
 
 export default Header
+
+const NavWrapper = styled.nav`
+  background: var(--lightBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+    padding-top: 1rem;
+  }
+`
