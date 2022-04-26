019154899ae06f63d495ff810c36ff7957902af1
isakbet <yosephbet@gmail.com>
2022-04-26 16:18:48 +0200

    export PageWrapper and Button styled component

---
+++ src/styledComponents.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2em;
+  margin-bottom: 2em;
+  padding: 1em;
+  background-color: var(--mainBlue);
+  background-color: #fafafa;
+`
+
+export const img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 3px;
+`
+
+export const h2 = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #000;
+  text-align: center;
+`
+
+export const p = styled.p`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #000;
+  text-align: center;
+`
+
+export const Button = styled.button`
+  background: ${(props) => (props.primary ? 'white' : 'lightblue')};
+  color: ${(props) => (props.primary ? 'blue' : 'mainDark')};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid var(--lightBlue);
+  border-radius: 3px;
+  text-decoration: none;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => (props.primary ? 'yellow' : 'black')};
+    color: ${(props) => (props.primary ? 'black' : 'yellow')};
+  }
+`
