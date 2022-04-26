5eac38b64ce117101298c84b07bb236f31a57998
isakbet <yosephbet@gmail.com>
2022-04-26 16:17:02 +0200

    add Home component, import PageWrapper

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageWrapper } from '../styledComponents'
+
+function Home() {
+  return (
+    <PageWrapper> 
+      <h1>Home</h1>      
+   </PageWrapper>
+  )
+}
+
+export default Home
