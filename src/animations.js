c6bc525a8284ce3144034cd76f944f7d1b97e9e8
isakbet <yosephbet@gmail.com>
2022-04-26 19:49:29 +0200

    export PageVariant, TitleVariant and TextVariant

---
+++ src/animations.js
@@ -0,0 +1,31 @@
+export const PageVariant = {
+  start: { opacity: 0 },
+  stop: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      when: 'beforeChildren',
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+export const TitleVariant = {
+  start: { x: '-100vw' },
+  stop: {
+    x: 0,
+    transition: {
+      type: 'spring',
+    },
+  },
+}
+
+export const TextVariant = {
+  start: { opacity: 0 },
+  stop: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+}
