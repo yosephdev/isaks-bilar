208e3e6054c7a26a03e642eef35343919fc03406
isakbet <yosephbet@gmail.com>
2022-04-28 15:12:34 +0200

    remove p , h2 and img styling

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -13,28 +13,6 @@ export const PageWrapper = styled.div`
   min-height: 100vh;
 `
 
-export const img = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center;
-  border-radius: 3px;
-`
-
-export const h2 = styled.h2`
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: #000;
-  text-align: center;
-`
-
-export const p = styled.p`
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #000;
-  text-align: center;
-`
-
 export const Button = styled.button`
   background: ${(props) => (props.primary ? 'white' : 'lightblue')};
   color: ${(props) => (props.primary ? 'blue' : 'mainDark')};
