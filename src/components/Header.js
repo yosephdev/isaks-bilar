3c6cc23998163ec22659abc0fec91fc241b27772
isakbet <yosephbet@gmail.com>
2022-04-28 15:13:15 +0200

    edit NavLink styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 function Header() {
   return (
-    <div>
+    <div >
       <NavLink to="/">Home</NavLink> <NavLink to="/products">Products</NavLink>
     </div>
   )
@@ -14,17 +14,18 @@ const NavLink = styled(Link)`
   color: var(--mainWhite);
   text-decoration: none;
   padding: 0.25em 1em;
+  border: 2px solid var(--lightBlue);
   margin: 1em;
-  font-size: 1em;
+  font-size: 1.25em;
   font-weight: bold;
-  text-transform: uppercase;
-  border: 1px solid var(--mainWhite);
+  text-transform: uppercase;  
+  cursor: pointer;
   border-radius: 3px;
   background-color: var(--mainBlue);
 
   &:hover {
     background-color: var(--mainYellow);
-    color: #000;
+    color: var(--mainDark);
     border: 1px solid var(--mainYellow);
   }
 `
