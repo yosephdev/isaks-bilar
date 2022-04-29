49b76dfdf57a50d6e34ef04da728460998d1a053
isakbet <yosephbet@gmail.com>
2022-04-29 20:29:27 +0200

    change PageWrapper and Button styling

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -5,29 +5,40 @@ export const PageWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 2em;
-  margin-bottom: 2em;
-  padding: 0 0.5rem;
-  background-color: var(--mainBlue);
-  background-color: #fafafa;
+  margin: 0 auto;
+  padding: 1em 2em;
   min-height: 100vh;
+  background-color: var(--main-background-color);
+  color: var(--main-text-color);  
 `
 
 export const Button = styled.button`
-  background: ${(props) => (props.primary ? 'white' : 'lightblue')};
-  color: ${(props) => (props.primary ? 'blue' : 'mainDark')};
+  background: ${(props) => (props.primary ? '#f9fbf2' : '#1e2019')};
+  color: ${(props) => (props.primary ? '#1e2019' : '#f9fbf2')};
   font-size: 1em;
-  /* margin: 1em;  */
+  margin: 1em;
+  text-align: center;
   padding: 0.25em 1em;
-  border: 2px solid var(--lightBlue);
+  border: 2px solid #fff;
   border-radius: 3px;
   text-decoration: none;
   font-weight: bold;
-  text-align: center;
   cursor: pointer;
 
   &:hover {
-    background: ${(props) => (props.primary ? 'yellow' : 'black')};
-    color: ${(props) => (props.primary ? 'black' : 'yellow')};
+    background: ${(props) => (props.primary ? '#fb8f67' : '#1e2019')};
+    color: ${(props) => (props.primary ? '#1e2019' : '#fb8f67')};
   }
 `
+
+
+
+  
+
+
+
+
+
+
+
+  
