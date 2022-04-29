7f83f3fb82b776e7390b0058949405e259274581
isakbet <yosephbet@gmail.com>
2022-04-29 20:33:45 +0200

    add Footer function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="page-footer font-small bg-blue pt-4">
+      <div className="container text-center text-md-left">
+        <div className="row">
+          <div className="col-md-6 mt-md-0 mt-3">
+            <h5 className="text-uppercase font-weight-bold">Contact Us</h5>
+            <p>You can contact us on 46-811-111-11</p>
+          </div>
+          <div className="col-md-6 mb-md-0 mb-3">
+            <h5 className="text-uppercase font-weight-bold">Return Policy</h5>
+            <p>We accept returns after 7 days max</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright text-center py-3">
+        © 2022 Copyright:
+        <span>Vito Ferrari</span>
+      </div>
+    </footer>
   )
 }
 
