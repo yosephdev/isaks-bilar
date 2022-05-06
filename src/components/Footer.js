f12ac72994e986245e17ee22ced7f34af60678ee
isakbet <yosephbet@gmail.com>
2022-05-06 14:57:03 +0200

    create Footer function, import FooterWrapper

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { FooterWrapper } from '../styledComponents'
+import { Link } from 'react-router-dom'
+import {
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+} from 'react-icons/ai'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <FooterWrapper>
+      <div className="footer-container">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Cars</Link>{' '}
+          </li>
+        </ul>
+        <div className="text">
+          <p>
+            Adeca is an all in one stop to fulfill your luxurious car needs.
+            Come and visit our store - we are open 7 days a week.
+          </p>
+          <p className="copyright">Copyright 2022. All Rights Reserved</p>
+        </div>
+
+        <div className="icon-container">
+          <i>
+            <AiFillFacebook />
+          </i>
+          <i>
+            <AiOutlineTwitter />
+          </i>
+          <i>
+            <AiOutlineInstagram />
+          </i>
+        </div>
+      </div>
+    </FooterWrapper>
   )
 }
 
