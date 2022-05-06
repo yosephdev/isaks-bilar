ecaee71f00914f4cfb70fe6a568c98a16cf3d8f0
isakbet <yosephbet@gmail.com>
2022-05-06 15:00:03 +0200

    add className to section tag

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -19,17 +19,17 @@ function Products() {
 
   return (
     <PageWrapper
-      as={motion.div}
+      as={motion.section}
       initial={{ x: '100vw', opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ type: 'spring', stiffness: 100 }}
     >
-      <section>
+      <section className="container">
         {products.map((product) => (
           <div key={product.id}>
             <img src={product.url} alt={product.url} />
             <h2>{product.title}</h2>
-            <p>{product.price}</p>
+            <p>Price: {product.price}</p>
             <Link to={`/products/${product.id}`}>
               <Button
                 as={motion.button}
