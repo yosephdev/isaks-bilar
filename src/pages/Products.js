27b85e46cb5eb06e128691bc8d2da1c0ea3fe32f
isakbet <yosephbet@gmail.com>
2022-06-09 15:18:55 +0200

    Remove Button style from styled components

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, PageWrapper } from '../styledComponents'
+import styled from 'styled-components'
+import { PageWrapper } from '../styledComponents'
 import { motion } from 'framer-motion'
+import { mobile } from '../responsive'
 
 function Products() {
   const [products, setProducts] = useState([])
@@ -24,7 +26,14 @@ function Products() {
       animate={{ x: 0, opacity: 1 }}
       transition={{ type: 'spring', stiffness: 100 }}
     >
-      <section className="container">
+      <CreateDiv>
+        <Link to={'/cars'}>
+          <ButtonAll>View All Cars</ButtonAll>
+        </Link>        
+      </CreateDiv>
+
+      <Header>Our Service</Header>
+      <Container>
         {products.map((product) => (
           <div key={product.id}>
             <img src={product.url} alt={product.url} />
@@ -44,9 +53,54 @@ function Products() {
             </Link>
           </div>
         ))}
-      </section>
+      </Container>
     </PageWrapper>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
+
+const Header = styled.h1`
+  text-align: center;
+  cursor: pointer;
+`
+
+const CreateDiv = styled.div`
+  text-align: right;
+  padding: 0 25px;
+  ${mobile({ margin: '20px 0' })}
+`
+
+const Button = styled.button`
+  padding: 12px 35px;
+  border: 1px solid white;
+  background-color: lightcoral;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: 500;
+  color: white;
+  &:hover {
+    background-color: lightslategrey;
+  }
+`
+
+const ButtonAll = styled.button`
+  padding: 12px 35px;
+  border: 1px solid white;
+  background-color: teal;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: 500;
+  color: white;
+  margin-right: 5px;
+  &:hover {
+    background-color: lightslategray;
+  }
+`
 
 export default Products
