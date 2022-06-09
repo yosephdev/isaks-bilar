c908279b6edc1f96bc13418e3125f54ddbb42a93
isakbet <yosephbet@gmail.com>
2022-06-09 15:16:49 +0200

    Change description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,133 @@
 import React from 'react'
-import { FooterWrapper } from '../styledComponents'
-import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { mobile } from '../responsive'
 import {
   AiFillFacebook,
   AiOutlineTwitter,
   AiOutlineInstagram,
+  AiFillMail,
+  AiOutlinePhone,
+  AiFillHome
+
 } from 'react-icons/ai'
 
 function Footer() {
   return (
-    <FooterWrapper>
-      <div className="footer-container">
-        <ul>
-          {/* <li>
-            <Link to="/">Home</Link>
-          </li> */}
-          <li>
-            <Link to="/products">Cars for Sale</Link>{' '}
-          </li>
-        </ul>
-        <div className="text">
-          <p>
-            Adeca is an all in one stop for your luxurious car needs.
-            Come and visit our store - we are open 7 days a week.
-          </p>
-          <p className="copyright">Copyright 2022. All Rights Reserved</p>
-        </div>
-
-        <div className="icon-container">
-          <i>
+    <Container>
+      <Left>
+        <Logo>Auto Mobil</Logo>
+        <Desc>
+          Auto Mobil is an online marketplace for automobiles, with Auto Mobil
+          users posting their cars for sale.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
             <AiFillFacebook />
-          </i>
-          <i>
-            <AiOutlineTwitter />
-          </i>
-          <i>
+          </SocialIcon>
+          <SocialIcon color="E4405F">
             <AiOutlineInstagram />
-          </i>
-        </div>
-      </div>
-    </FooterWrapper>
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <AiOutlineTwitter />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>About us</ListItem>
+          <ListItem>Contact</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <AiFillHome style={{ marginRight: '10px' }} /> Eskilstuna, Sweden      
+        </ContactItem>
+        <ContactItem>
+          <AiOutlinePhone style={{ marginRight: '10px' }} /> +46 703566108
+        </ContactItem>
+        <ContactItem>
+          <AiFillMail style={{ marginRight: '10px' }} /> yosephbet@gmail.com
+        </ContactItem>
+      </Right>
+    </Container>
   )
 }
 
+
+const Container = styled.div`
+  margin-top: 70px;
+  display: flex;
+  ${mobile({ flexDirection: 'column' })}
+`
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+
+const Logo = styled.h1``
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`
+
+const SocialContainer = styled.div`
+  display: flex;
+`
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: 'none' })}
+`
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: '#fff8f8' })}
+`
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`
+
+const Payment = styled.img`
+  width: 50%;
+`
 export default Footer
