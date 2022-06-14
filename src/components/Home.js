7605f3f0dee2afcbb77f0b606a2853c774c793d3
isakbet <yosephbet@gmail.com>
2022-06-14 17:38:57 +0200

    Change PageWrapper, PageVariant

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { PageWrapper } from '../styledComponents'
+import Cars from '../pages/Cars'
 import { motion } from 'framer-motion'
-import { PageVariant, TitleVariant, TextVariant } from '../animations.js'
+const PageWrapper = motion.div
+const PageVariant = {
+  initial: {
+    opacity: 0,
+    y: -100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+}
 
 function Home() {
   return (
-    <PageWrapper
-      as={motion.div}
-      initial="start"
-      animate="stop"
-      variants={PageVariant}
-    >
-      <motion.h1 variants={TitleVariant}>
-        <span>Welcome to</span>
-        <br></br>
-        <span>Cars for Sale</span>
-      </motion.h1>
-
-     <motion.h2 variants={TextVariant}>
-        <span>
-          We have a wide range of cars for sale, from used cars to new cars.
-        </span>
-      </motion.h2>
-
-      
+    <PageWrapper>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={PageVariant}
+      >
+        <Cars />
+      </motion.div>
     </PageWrapper>
   )
 }
