529211d476f7a20895f10718dc1b3d48ec18abdd
Yoseph Berhane <yosephbet@gmail.com>
2023-03-10 15:39:09 +0100

    change car info api

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
-import Item from './Item.js'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Item from './Item'
 import './Products.css'
-import { data } from './data.js'
 
 const Products = (props) => {
-  const [items] = useState(data)
+  const { make, year, model } = useParams()
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const url = `https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=${make}&year=${year}&model=${model}`
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setItems(data.Trims))
+  }, [make, year, model])
+
   return (
     <div className="prods">
-      {items.map((item, i) => {
-        if (item.categoryId === props.catId) {
-          return (
-            <Item
-              key={i}
-              image={item.image}
-              prodName={item.productName}
-              price={item.price}
-            />
-          )
-        }
-      })}
+      {items.map((item, i) => (
+        <Item
+          key={i}
+          image={item.model_img_url}
+          prodName={`${item.model_name} ${item.model_trim}`}
+          price={item.model_msrp}
+          onClick={() => props.addToCart(item.model_id)}
+        />
+      ))}
     </div>
   )
 }
