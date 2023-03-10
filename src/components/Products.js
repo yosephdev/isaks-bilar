eda2eea604c2589d6a0c9f823b819b380d9eaa5b
Yoseph Berhane <yosephbet@gmail.com>
2023-03-10 17:01:25 +0100

    change api request

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,11 +8,32 @@ const Products = (props) => {
   const [items, setItems] = useState([])
 
   useEffect(() => {
+    // Debugging statement to log the URL being requested
+    console.log(
+      `Requesting URL: https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=${make}&year=${year}&model=${model}`
+    )
     const url = `https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=${make}&year=${year}&model=${model}`
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => setItems(data.Trims))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        if (data.Trims) {
+          setItems(data.Trims)
+        } else {
+          throw new Error('Trims data was not found in the response')
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching trims data:', error)
+      })
   }, [make, year, model])
+
+  // Debugging statement to log the items state
+  console.log('Items:', items)
 
   return (
     <div className="prods">
