2f72b90fad91b2568529c10dccd4ad803e1d7d4b
Yoseph Berhane <yosephbet@gmail.com>
2023-03-10 17:02:05 +0100

    change title name

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,37 +3,33 @@ import Products from "./Products";
 import Intro from "./Intro";
 import "./Home.css";
 import Shipping from "./Shipping";
+import ProductsList from "./ProductsList";
 
 const Homepage = () => {
   return (
     <div className="home">
       <Intro />
       <Shipping />
-
       <div className="home__container">
         <div className="home__title">
           <h1 className="category-name">
-            Nya bilar <span className="category-name__underline"></span>                      
+            Nya bilar <span className="category-name__underline"></span>
           </h1>
-          <Products catId={1}></Products>
+          <ProductsList catId={1} catName="Nya bilar" />
         </div>
 
         <div className="home__title">
-          <h1 className="category-name">
-            Gammla bilar
-          </h1>
-          <Products catId={2}></Products>
+          <h1 className="category-name">Gammla bilar</h1>
+          <ProductsList catId={2} catName="Gammla bilar" />
         </div>
 
         <div className="home__title">
-          <h1 className="category-name">
-            Bilar för barn           
-          </h1>
-          <Products catId={3}></Products>
+          <h1 className="category-name">Barn Bilar</h1>
+          <ProductsList catId={3} catName="Barn Bilar" />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Homepage;
