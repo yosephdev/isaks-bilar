7cd326a64c18ce523319fd2051a71fa7836179c6
Yoseph Berhane <yosephbet@gmail.com>
2023-03-29 16:41:32 +0200

    add make year

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ import { useStateValue } from './StateProvider'
 import './Button.css'
 import './Item.css'
 
-const Item = ({ image, prodName, price, modelId, addToCart }) => {
+const Item = ({ image, prodName, price, modelId, addToCart, make, year }) => {
   const [{ basket }, dispatch] = useStateValue()
 
   const handleClick = () => {
@@ -14,6 +14,8 @@ const Item = ({ image, prodName, price, modelId, addToCart }) => {
         image: image,
         prodName: prodName,
         price: price,
+        make: make,
+        year: year,
       },
     })
   }
@@ -22,6 +24,7 @@ const Item = ({ image, prodName, price, modelId, addToCart }) => {
     <div className="item">
       <img className="prod prod-image" src={image} alt={prodName} />
       <p className="prod prod-name">{prodName}</p>
+      <p className="prod prod-year">{year}</p>
       <p className="prod prod-price">{price} kr</p>
       <button className="buy-button" onClick={handleClick}>
         Köp
