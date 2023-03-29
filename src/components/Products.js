7ba54ed3da2a1b51540c38ee9e88e2a1f204fdc0
Yoseph Berhane <yosephbet@gmail.com>
2023-03-29 16:40:45 +0200

    change api

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,16 +4,17 @@ import Item from './Item'
 import './Products.css'
 
 const Products = (props) => {
-  const { make, year, model } = useParams()
+  const { make, model } = useParams()
   const [items, setItems] = useState([])
+  const apiKey = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
-    // Debugging statement to log the URL being requested
-    console.log(
-      `Requesting URL: https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=${make}&year=${year}&model=${model}`
-    )
-    const url = `https://www.carqueryapi.com/api/0.3/?cmd=getTrims&make=${make}&year=${year}&model=${model}`
-    fetch(url)
+    const url = `https://api.api-ninjas.com/v1/car-models?make=${make}&model=${model}&year=latest`
+    fetch(url, {
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok')
@@ -21,29 +22,29 @@ const Products = (props) => {
         return response.json()
       })
       .then((data) => {
-        if (data.Trims) {
-          setItems(data.Trims)
+        if (data.length > 0) {
+          setItems(data)
         } else {
-          throw new Error('Trims data was not found in the response')
+          throw new Error('No car models found for the given make and model')
         }
       })
       .catch((error) => {
-        console.error('Error fetching trims data:', error)
+        console.error('Error fetching car models data:', error)
       })
-  }, [make, year, model])
-
-  // Debugging statement to log the items state
-  console.log('Items:', items)
+  }, [make, model])
 
   return (
     <div className="prods">
       {items.map((item, i) => (
         <Item
           key={i}
-          image={item.model_img_url}
-          prodName={`${item.model_name} ${item.model_trim}`}
-          price={item.model_msrp}
-          onClick={() => props.addToCart(item.model_id)}
+          image={`https://api.api-ninjas.com${item.imageUrl}`}
+          prodName={`${props.make} ${item.prodName}`}
+          price={item.price}
+          modelId={item.modelId}
+          // addToCart={addToCart}
+          make={props.make}
+          year={`${item.year}`}
         />
       ))}
     </div>
