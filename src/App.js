a803de7f677c129e1ad54c533752e504c22e2435
Yoseph Berhane <yosephbet@gmail.com>
2023-03-29 16:41:49 +0200

    add products path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Homepage from './components/Homepage'
 import ProductsList from './components/ProductsList'
+import Products from './components/Products'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Checkout from './components/Checkout'
 import About from './components/About'
@@ -16,38 +17,31 @@ function App() {
     <BrowserRouter>
       <div>
         <Header />
-      </div>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/customer" element={<CustomerService />} />
-          <Route exact path="/howto" element={<HowTo />} />
-          <Route exact path="/club" element={<Club />} />
-          <Route path="/" element={<Homepage />} />
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/customer" element={<CustomerService />} />
+            <Route exact path="/howto" element={<HowTo />} />
+            <Route exact path="/club" element={<Club />} />
 
-          <Route
-            path="/gammalbilar"
-            exact
-            element={<ProductsList catName="Gammalbilar" catId={2} />}
-          />
-
-          <Route
-            path="/nyabilar"
-            exact
-            element={<ProductsList catName="Nyabilar" catId={1} />}
-          />
-
-          <Route
-            path="/smabilar"
-            exact
-            element={<ProductsList catName="Bilar till barn" catId={3} />}
-          />
-          <Route path="/varukorg" exact element={<Checkout />} />
-        </Routes>
-      </div>
-
-      <div>
+            <Route
+              path="/gammalbilar"
+              element={<ProductsList catName="Gammalbilar" catId={2} />}
+            />
+            <Route
+              path="/nyabilar"
+              element={<ProductsList catName="Nyabilar" catId={1} />}
+            />
+            <Route
+              path="/smabilar"
+              element={<ProductsList catName="Bilar till barn" catId={3} />}
+            />
+            <Route path="/products/:make/:model" element={<Products />} />
+            <Route path="/varukorg" element={<Checkout />} />
+            <Route path="/" element={<Homepage />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </BrowserRouter>
