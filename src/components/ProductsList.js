8c54bd0b0478b5512458cbeddb7ea62b7240536c
Yoseph Berhane <yosephbet@gmail.com>
2023-03-29 16:40:16 +0200

    change json data

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
-import { designImage } from './data'
+import React, { useState, useEffect } from 'react'
 import Products from './Products'
 import './Productslist.css'
 
 const ProductsList = (props) => {
-  const [image] = useState(designImage)
+  const [image, setImage] = useState([])
   const categoryIds = [props.catId, props.catId + 1] // define an array of category IDs
+  const apiKey = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    const url = `https://api.api-ninjas.com/v1/car-images?make=${props.make}&model=${props.model}&year=latest`
+    fetch(url, {
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        if (data.length > 0) {
+          setImage(data)
+        } else {
+          throw new Error('No car images found for the given make and model')
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching car images data:', error)
+      })
+  }, [props.make, props.model])
+
   return (
     <div className="list__container">
       <div className="page__title">
@@ -15,14 +41,20 @@ const ProductsList = (props) => {
       <div className="page__image">
         {image.map((n, i) => {
           if (n.categoryId === props.catId) {
-            return <img className="image" key={i} src={n.image}></img>
+            return <img className="image" key={i} src={n.imageUrl}></img>
           }
         })}
       </div>
+
       <div className="products">
-        {/* render Products component with different catId props */}
+        {/* render Products component with different make and model props */}
         {categoryIds.map((categoryId) => (
-          <Products key={categoryId} catId={categoryId} />
+          <Products
+            key={categoryId}
+            catId={categoryId}
+            make={props.make}
+            model={props.model}
+          />
         ))}
       </div>
     </div>
