33f27425b4740d727dd0193d9e04465f756b704f
Yoseph Berhane <yosephbet@gmail.com>
2023-03-30 20:25:25 +0200

    remove car make and year

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,32 +1,28 @@
 import React from 'react'
-import { useStateValue } from './StateProvider'
 import './Button.css'
 import './Item.css'
+import { useStateValue } from './StateProvider'
 
-const Item = ({ image, prodName, price, modelId, addToCart, make, year }) => {
+const Item = ({ image, prodName, price }) => {
   const [{ basket }, dispatch] = useStateValue()
 
-  const handleClick = () => {
-    addToCart(modelId)
+  const addToBasket = () => {
     dispatch({
       type: 'ADD_TO_BASKET',
       item: {
         image: image,
         prodName: prodName,
         price: price,
-        make: make,
-        year: year,
       },
     })
   }
 
   return (
     <div className="item">
-      <img className="prod prod-image" src={image} alt={prodName} />
+      <img className="prod prod-image" src={image}></img>
       <p className="prod prod-name">{prodName}</p>
-      <p className="prod prod-year">{year}</p>
       <p className="prod prod-price">{price} kr</p>
-      <button className="buy-button" onClick={handleClick}>
+      <button className="buy-button" onClick={addToBasket}>
         Köp
       </button>
     </div>
