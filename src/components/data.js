c3d074425887c5f94d7f25a4e8a82e3d06b9ccab
Yoseph Berhane <yosephbet@gmail.com>
2023-03-30 20:27:12 +0200

    change images

--- src/components/data.js
+++ src/components/data.js
@@ -2,84 +2,84 @@ export const data = () => {
   return [
     {
       productId: 1,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild1.jpg',
       productName: 'Product 1',
       price: 349,
       categoryId: 1,
     },
     {
       productId: 2,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild2.jpg',
       productName: 'Product 2',
       price: 149,
       categoryId: 1,
     },
     {
       productId: 3,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild3.jpg',
       productName: 'Product 3',
       price: 229,
       categoryId: 1,
     },
     {
       productId: 4,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild4.jpg',
       productName: 'Product 4',
       price: 349,
       categoryId: 1,
     },
     {
       productId: 5,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild5.jpg',
       productName: 'Product 5',
       price: 499.99,
       categoryId: 2,
     },
     {
       productId: 6,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild6.jpg',
       productName: 'Product 6',
       price: 290,
       categoryId: 2,
     },
     {
       productId: 7,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild7.jpg',
       productName: 'Product 7',
       price: 199.99,
       categoryId: 2,
     },
     {
       productId: 8,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild8.jpg',
       productName: 'Product 8',
       price: 249,
       categoryId: 2,
     },
     {
       productId: 9,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild9.jpg',
       productName: 'Product 9',
       price: 449.99,
       categoryId: 3,
     },
     {
       productId: 10,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild10.jpg',
       productName: 'Product 10',
       price: 219,
       categoryId: 3,
     },
     {
       productId: 11,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild11.jpg',
       productName: 'Product 11',
       price: 395.99,
       categoryId: 3,
     },
     {
       productId: 12,
-      image: 'https://picsum.photos/200/300',
+      image: '/images/Bild12.jpg',
       productName: 'Product 12',
       price: 559.99,
       categoryId: 3,
@@ -87,43 +87,3 @@ export const data = () => {
   ]
 }
 
-export const designImage = () => {
-  return [
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 2,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 2,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 2,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 3,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 3,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 3,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 1,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 1,
-    },
-    {
-      image: 'https://picsum.photos/200/300',
-      categoryId: 1,
-    },
-  ]
-}
